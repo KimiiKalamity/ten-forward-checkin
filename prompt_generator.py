5e6092b085f6d9ca6970bf4114de2ef82f0a74d5
KimiiKalamity <kimberly01241980@gmail.com>
2026-04-12 20:34:02 -0700

    Add prompts to generator script

--- prompt_generator.py
+++ prompt_generator.py
@@ -1,6 +1,9 @@
 import random
 
 prompts = [
+    "What would make tonight feel complete?"
+    "What am I avoiding that might actually be small?"
+    "What would Picard do today?"
     "What am I making this mean about myself?",
     "What feels heavy right now?",
     "What feels quietly hopeful right now?",
